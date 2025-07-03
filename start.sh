4145a78a4a156dcb31408b252241aed367659b7d
mhavliczek <mhavliczek@gmail.com>
2025-07-02 23:36:57 -0400

    de cero

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH="/usr/local/bin:$PATH"
-
-# Generar datos de ejemplo
-python src/data_generator.py
-
-# Iniciar la aplicación
-streamlit run src/app.py --server.port=$PORT --server.address=127.0.0.1 
